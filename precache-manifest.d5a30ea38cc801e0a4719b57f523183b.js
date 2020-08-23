b6645e7f91f7ef570b5e67e36b9b01c1caf26da2
bharathjinka09 <bharathjinka09@gmail.com>
2020-08-23 17:27:05 +0530

    Updates

---
+++ precache-manifest.d5a30ea38cc801e0a4719b57f523183b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3646d1bd10d9c5136f5c4ef6ae112d55",
+    "url": "/bootstrap-5-with-react/index.html"
+  },
+  {
+    "revision": "3031ff7dc9ed5988f6b1",
+    "url": "/bootstrap-5-with-react/static/css/main.830da0c5.chunk.css"
+  },
+  {
+    "revision": "216edfd38f51f87c39bc",
+    "url": "/bootstrap-5-with-react/static/js/2.a6710da0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bootstrap-5-with-react/static/js/2.a6710da0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3031ff7dc9ed5988f6b1",
+    "url": "/bootstrap-5-with-react/static/js/main.034ee217.chunk.js"
+  },
+  {
+    "revision": "bf790e0ab86297d41549",
+    "url": "/bootstrap-5-with-react/static/js/runtime-main.570b6c6c.js"
+  }
+]);
